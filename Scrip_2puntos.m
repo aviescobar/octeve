cb7d47d0fb70b436298e6e827911380ef632bad5
aviescobar <91568563+aviescobar@users.noreply.github.com>
2021-10-28 19:51:43 -0500

    Scrip de dos puntos
    Scrip para recordar las funciones real

---
+++ Scrip_2puntos.m
@@ -0,0 +1,20 @@
+% Octave Script
+% Title            :Funcion Real de variable 
+% Descrption       :scrip 
+% Authors          :Victoria Avigail Escobar Noguez 
+% Date             :202123419
+% Version          :1
+% Usage			       :octave> /Documents/octave 
+% Notes			       :Se requiere aplicacion Octave
+
+clear
+%defina de la funcion
+r=-50:0.1:50;
+f=(2+x.^2+3+x)/(x.^2+4*x*5);
+f= (syms)  
+2*x^2+3*x
+-----------
+"x2+4*x+50"
+
+ezplot=(f)
+ 
