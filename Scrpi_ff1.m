fa00a4abbb84f230fe5a488e60f3b00fca1d0b56
aviescobar <91568563+aviescobar@users.noreply.github.com>
2021-11-25 21:08:43 -0600

    funcio 1
    resolucion del ejercicio

---
+++ Scrpi_ff1.m
@@ -0,0 +1,10 @@
+%Octave Script
+%Title          :Grafica 1
+%Author         :Vctoria Avigail Escobar Noguez 
+%Description    :Graficar una funcion trozos
+%Date           :202123801
+%Version        :1
+%notes          :Requiere aplicacion de octave, usar su linea de comandos
+%Date            20211125
+%                Grupo:3102
+
